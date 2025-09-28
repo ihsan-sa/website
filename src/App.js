b9b4e63237417be8455f45e14f12e894a20d75ba
Ihsan S. <ihsan.salari@gmail.com>
2025-09-27 18:09:58 -0700

    changed colour

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello, I'm <span style={{ color: '#c792ea' }}>Ihsan</span>.
+          Hello, I'm <span style={{ color: '#82aaff' }}>Ihsan</span>.
         </p>
         <p style={{fontSize: '17px'}}>
           Website under construction.
           Shoot me a message at <a href="mailto:hi@ihsan.cc" style={{color: '#d06085', textDecoration: 'none' }}>hi@ihsan.cc</a>
-        </p>
-        <p style={{fontSize: '17px'}}>
+          <div style={{ height: '5px' }} />
           Check out my projects: <a href="https://docs.ihsan.cc" style={{color: '#d06085', textDecoration: 'none' }}>docs.ihsan.cc</a>
         </p>
       </header>
